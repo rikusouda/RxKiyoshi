e1cd250fc7adcdda3f1bfe6b675f96c5c5fa0f7e
Yuki Yoshioka <rikusouda@users.noreply.github.com>
2017-05-07 14:33:41 +0900

    ひとまずRealmに履歴を保存するようにした

--- RxKiyoshi/KiyoshiViewModel.swift
+++ RxKiyoshi/KiyoshiViewModel.swift
@@ -8,17 +8,24 @@
 
 import Foundation
 import RxSwift
+import RealmSwift
 
 
 class KiyoshiViewModel {
-    enum ZunDoko {
+    enum ZunDoko2: Int {
         case ズン
         case ドコ
         case キヨシ
     }
     
-    var history: [ZunDoko] = [.ドコ, .ドコ, .ドコ, .ドコ, .ドコ]
+    var history: [ZunDoko2] = [.ドコ, .ドコ, .ドコ, .ドコ, .ドコ]
     let outputString = PublishSubject<String>()
+    
+    init() {
+        DispatchQueue.main.async { [weak self] in
+            self?.reloadData()
+        }
+    }
     
     func addZun() {
         add(next: .ズン)
@@ -26,14 +33,41 @@ class KiyoshiViewModel {
     
     func addDoko() {
         add(next: .ドコ)
+        
+        let realm = try! Realm()
+        try! realm.write() {
+            let newObject = ZunDoko.create(val: ZunDoko.Zdk.zun)
+            realm.add(newObject)
+        }
     }
     
-    private func add(next: ZunDoko) {
+    private func add(next: ZunDoko2) {
         history = history[1...4] + [next]
         outputString.onNext("\(next)")
         
+        let realm = try! Realm()
+        try! realm.write() {
+            if let zdk = ZunDoko.Zdk(rawValue: next.rawValue) {
+                let newObject = ZunDoko.create(val: zdk)
+                realm.add(newObject)
+            }
+        }
+        
         if history == [.ズン, .ズン, .ズン, .ズン, .ドコ] {
             add(next: .キヨシ)
+        }
+    }
+    
+    func reloadData() {
+        history = [.ドコ, .ドコ, .ドコ, .ドコ, .ドコ]
+        
+        let realm = try! Realm()
+        let zdks = realm.objects(ZunDoko.self).sorted(byKeyPath: "id")
+        
+        for zdk in zdks {
+            let zdk2 = ZunDoko2(rawValue: zdk.valAsZdk.rawValue)!
+            history = history[1...4] + [zdk2]
+            outputString.onNext("\(zdk2)")
         }
     }
     
---
+++ RxKiyoshi/ZunDoko.swift
@@ -0,0 +1,44 @@
+//
+//  ZunDoko.swift
+//  RxKiyoshi
+//
+//  Created by Yuki Yoshioka on 2017/05/07.
+//  Copyright © 2017年 Yuki Yoshioka. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+
+class ZunDoko: Object {
+    public enum Zdk: Int {
+        case zun = 0
+        case doko = 1
+        case kiyoshi = 2
+    }
+    
+    dynamic var id: Int = 0
+    dynamic var val: Int = 0
+    
+    var valAsZdk: Zdk {
+        return Zdk(rawValue: self.val)!
+    }
+    
+    static func create(val: Zdk) -> ZunDoko {
+        let z = ZunDoko()
+        
+        do {
+            let defaults = UserDefaults.standard
+            let currentId = defaults.integer(forKey: "ZunDokoId")
+            let newId = currentId + 1
+            z.id = newId
+            defaults.set(newId, forKey: "ZunDokoId")
+        }
+        
+        z.val = val.rawValue
+        return z
+    }
+    
+    override static func primaryKey() -> String? {
+        return "id"
+    }
+}
